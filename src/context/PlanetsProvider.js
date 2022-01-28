b2f3d351ea3468d39644f1c7a510b3457cf5789b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-28 20:54:25 -0300

    Alteração na forma de passagem dos states e setStates, criando o objeto data

--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -6,6 +6,9 @@ import fetchApi from '../service/requestApi';
 function PlanetsProvider({ children }) {
   const [planetList, setPlanetList] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
+  const [options, setOptions] = useState('population');
+  const [compare, setCompare] = useState('Maior que');
+  const [amount, setAmount] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -15,15 +18,27 @@ function PlanetsProvider({ children }) {
   }, []);
   // configuração com async await aprendida com o instrutor Arthur no horário da mentoria.
 
+  const data = {
+    planetList,
+    nameFilter,
+    setNameFilter,
+    options,
+    setOptions,
+    compare,
+    setCompare,
+    amount,
+    setAmount,
+  };
+
   return (
-    <planetsContext.Provider value={ { data: planetList, nameFilter, setNameFilter } }>
+    <planetsContext.Provider value={ data }>
       {children}
     </planetsContext.Provider>
   );
 }
 
 PlanetsProvider.propTypes = {
-  children: PropTypes.objectOf(PropTypes.children).isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default PlanetsProvider;
