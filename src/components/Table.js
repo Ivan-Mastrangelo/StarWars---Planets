b1293795b9d0a0ff354f9371688da8a11eae58c2
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-28 20:52:17 -0300

    Alteração na recepção dos state e setState e inclusão do método toLowerCase no filtro por nome

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import planetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data, nameFilter } = useContext(planetsContext);
+  const { planetList, nameFilter } = useContext(planetsContext);
 
   return (
     <table>
@@ -25,24 +25,25 @@ function Table() {
       </thead>
       <tbody>
         {
-          data.filter((planet) => planet.name.includes(nameFilter)).map((planet) => (
-            <tr key={ planet.name }>
-
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.population}</td>
-              <td>{planet.films}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-              <td>{planet.url}</td>
-            </tr>
-          ))
+          planetList.filter((planet) => planet.name.toLowerCase()
+            .includes(nameFilter.toLowerCase()))
+            .map((planet) => (
+              <tr key={ planet.name }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))
         }
       </tbody>
     </table>
