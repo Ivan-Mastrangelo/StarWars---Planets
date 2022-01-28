129a361612fe1c96285ba309e23a8841a9064751
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-28 12:50:47 -0300

    Insere o componente barra de filtros ao componente app com sua importação

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
+import FiltersBar from './components/FilterBar';
 
 function App() {
   return (
     <PlanetsProvider>
+      <FiltersBar />
       <Table />
     </PlanetsProvider>
   );
