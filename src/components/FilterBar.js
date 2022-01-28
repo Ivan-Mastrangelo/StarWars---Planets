1d5d715b0e81162c881e2f93aad373ea804810e2
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-28 20:49:28 -0300

    Implementação dos selects e do button, com passagem das função que setam os estados para a linha dos onChanges. E alteração na forma de recepção dos estados e métodos

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,11 +2,20 @@ import React, { useContext } from 'react';
 import planetsContext from '../context/PlanetsContext';
 
 function FiltersBar() {
-  const { setNameFilter, nameFilter } = useContext(planetsContext);
+  const {
+    setNameFilter,
+    nameFilter,
+    options,
+    setOptions,
+    compare,
+    setCompare,
+    amount,
+    setAmount,
+  } = useContext(planetsContext);
 
-  const handleChange = ({ target }) => {
-    setNameFilter(target.value);
-  };
+  submitFilters((e) => {
+    e.preventDefault();
+  });
 
   return (
     <form>
@@ -17,9 +26,54 @@ function FiltersBar() {
           type="text"
           data-testid="name-filter"
           value={ nameFilter }
-          onChange={ handleChange }
+          onChange={ (e) => setNameFilter(e.target.value) }
         />
       </label>
+      <label htmlFor="options">
+        <select
+          id="options"
+          data-testid="column-filter"
+          value={ options }
+          onChange={ (e) => { setOptions(e.target.value); } }
+        >
+          Filtrar por
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+      </label>
+      <label htmlFor="compare">
+        <select
+          id="compare"
+          data-testid="comparison-filter"
+          value={ compare }
+          onChange={ (e) => { setCompare(e.target.value); } }
+        >
+          Grau de comparação
+          <option value="maior que">maior que</option>
+          <option value="igual a">igual a</option>
+          <option value="menor que">menor que</option>
+        </select>
+      </label>
+      <label htmlFor="amount">
+        <input
+          type="number"
+          id="amount"
+          data-testid="value-filter"
+          value={ amount }
+          onChange={ (e) => { setAmount(e.target.value); } }
+        />
+        Quantidade
+      </label>
+      <button
+        type="submit"
+        onClick={ submitFilters }
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
     </form>
   );
 }
