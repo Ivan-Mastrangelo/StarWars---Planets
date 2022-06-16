8af6ef42375e2419dbe3996ede9ae1d68134c16b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-16 20:59:34 -0300

    feat: inserir o header no App

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import PlanetsProvider from './context/PlanetsProvider';
 import Table from './components/Table';
 import FiltersBar from './components/FilterBar';
+import Header from './components/Header';
 
 function App() {
   return (
     <PlanetsProvider>
+      <Header />
       <FiltersBar />
       <Table />
     </PlanetsProvider>
