7cc2d39b3f0ab83765fd30df01f6d0b2b567c609
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-06-16 23:59:39 -0300

    feat: implementar o Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <p className="footer">A imagem do espaço utililizada como background foi baixada sem restrições do site https://wall.alphacoders.com/. A imagem da espaço nave Millennium Falcon foi baixada sem restrições do Google. Entendo assim que são de domínio público. Em caso contrário, basta me avisarem que retiro imediatamente.</p>;
+}
+
+export default Footer;
